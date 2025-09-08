b72e8e1e210d61388758f70c762ab7fd4d9a812c
karmilan <delftkarmilan@gmail.com>
2025-09-08 21:16:41 +0530

    feat: add express route for root endpoint and update package.json for nodemon

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,5 +2,9 @@ import express from 'express';
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json({ "msg": 'success' })
+})
+
 app.listen(5000, () => (console.log('kar running')
 ))
