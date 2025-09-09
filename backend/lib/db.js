e16c104faef93f6398d43a65afaab17795b5d72d
karmilan <delftkarmilan@gmail.com>
2025-09-09 14:25:06 +0530

    db connected

---
+++ backend/lib/db.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MongoDB Connected...");
+    } catch (error) {
+        console.error('err', error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,20 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from '../lib/db.js';
 
+dotenv.config()
 const app = express()
+const PORT = process.env.PORT || 5000
+
+// Connect MongoDB
+connectDB();
 
 app.get('/', (req, res) => {
     res.json({ "msg": 'success' })
 })
 
-app.listen(5000, () => (console.log('kar running')
-))
+
+
+app.listen(PORT, () => {
+    console.log(`The server is running at PORT:${PORT}`);
+});
