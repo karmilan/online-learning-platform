39b357b3aadcb3adda1e2aa8191333041fc7ab17
karmilan <delftkarmilan@gmail.com>
2025-09-10 19:33:40 +0530

    feat(frontend): initialized with react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <main className="relative bg-gradient-to-r from-emerald-900 to-emerald-700 text-white">
+        <h1 className="text-3xl font-bold tracking-tight">
+          🚀 React + Vite + Tailwind Starter
+        </h1>
+        <p className="mt-2 text-gray-600">
+          Clean and professional frontend setup ready to build.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default App;
