5ee5e583d00af76b1237002d4d8a7d85fe1f49ae
karmilan <delftkarmilan@gmail.com>
2025-09-10 13:46:50 +0530

    feat(middleware): add verify token and rbac

--- backend/src/routes/courses.route.js
+++ backend/src/routes/courses.route.js
@@ -5,12 +5,19 @@ import {
   getAllCourses,
   updateCourse,
 } from "../controllers/courses.controller.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
+import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
-router.post("/courses", createCourse);
-router.get("/courses", getAllCourses);
-router.put("/courses/:id", updateCourse);
-router.delete("/courses/:id", deleteCourse);
+router.post("/courses", verifyToken, authorizeRoles("admin"), createCourse);
+router.get("/courses", verifyToken, getAllCourses);
+router.put("/courses/:id", verifyToken, authorizeRoles("admin"), updateCourse);
+router.delete(
+  "/courses/:id",
+  verifyToken,
+  authorizeRoles("admin"),
+  deleteCourse
+);
 
 export default router;
--- backend/src/routes/users.route.js
+++ backend/src/routes/users.route.js
@@ -5,12 +5,14 @@ import {
   getAllUsers,
   updateUser,
 } from "../controllers/users.controller.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
+import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
-router.post("/users", createUser);
-router.get("/users", getAllUsers);
-router.put("/users/:id", updateUser);
-router.delete("/users/:id", deleteUser);
+router.post("/users", verifyToken, authorizeRoles("admin"), createUser);
+router.get("/users", verifyToken, authorizeRoles("admin"), getAllUsers);
+router.put("/users/:id", verifyToken, authorizeRoles("admin"), updateUser);
+router.delete("/users/:id", verifyToken, authorizeRoles("admin"), deleteUser);
 
 export default router;
